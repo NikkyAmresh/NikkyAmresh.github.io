947c1032f96434e20cb6e0be13eb0c8807304a0f
Nikky Amresh <nikkyamresh8@gmail.com>
2020-12-09 19:41:24 +0530

    added file

--- js/chunk-2d21b23e.4cf69586.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21b23e"],{bf04:function(e,n,l){"use strict";l.r(n);var c=function(){var e=this,n=e.$createElement,l=e._self._c||n;return l("div",[e._v(" Hello World ")])},t=[],u=l("0c7c"),o={},r=Object(u["a"])(o,c,t,!1,null,null,null);n["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d21b23e.4cf69586.js.map
